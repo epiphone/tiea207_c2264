7c004235d30e4ba1a602f9083cb5c3bbf27d6fb6
Aleksi Pekkala <aleksipekkala@hotmail.com>
2013-04-10 09:02:46 +0300

    Arkkitehtuurikuvaa yms.

--- scraper_wrapper.py
+++ scraper_wrapper.py
@@ -5,6 +5,7 @@
 # import mh_scraper
 # import vr_scraper
 # import auto_scraper
+from thread_helper import do_threaded_work
 
 
 class VRScraper:
@@ -59,15 +60,44 @@ class MHScraper:
 class AutoScraper:
     """Tilapäinen dummy-luokka."""
     def hae_matka(*args, **kwargs):
-        return [
-            {"lahtoaika": "2013-04-05 16:59",
+        return {
+            "lahtoaika": "2013-04-05 16:59",
             "saapumisaika": "2013-04-05 22:22",
-            "km":356,
+            "km": 356,
+            "url": "http://linkki-kartalle.fi"
+        }
 
-        ]
+
+class BensaScraper:
+    """Tilapäinen dummy-luokka."""
+    def hae_hinta(*args, **kwargs):
+        return [1.672, 1.721, 1.555]
 
 
-def hae_matka(mista=None, mihin=None, lahtoaika=None, saapumisaika=None,
-    bussi=True, juna=True, auto=True):
-    """Palauttaa valitulle matkalle eri yhteydet."""
-    pass  # TODO
+class ScraperWrapper:
+    """Käärii scraperit yhden rajapinnan alle."""
+
+    def __init__(self):
+        self.vr_scraper = VRScraper()
+        self.mh_scraper = MHScraper()
+        self.auto_scraper = AutoScraper()
+        self.bensa_scraper = BensaScraper()
+        self.scraperit = [self.vr_scraper, self.mh_scraper,
+            self.auto_scraper]
+
+    def hae_matka(self, mista=None, mihin=None, lahtoaika=None, saapumisaika=None,
+        bussi=True, juna=True, auto=True, alennusluokka=0):
+        """Palauttaa valitulle matkalle eri yhteydet."""
+        def f(scraper):
+            # TODO: tsekkaa cache
+            return scraper.hae_matka(mista, mihin, lahtoaika, saapumisaika)
+            # TODO: aseta cache
+
+        scraperit = zip(self.scraperit, [juna, bussi, auto])
+        scraperit = [s for s, b in scraperit if b]
+
+        results = do_threaded_work(scraperit, f)
+        return results
+
+
+wrapper = ScraperWrapper()
