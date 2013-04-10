f2415bb91fa733dc98c09cac22fdb0c38a22c315
Juuseri <jupeliks@hotmail.com>
2013-04-10 12:04:36 +0300

    Vaihdoin Olion kayton Dictionaryyn ja Lisasin aakkosien vaihdon urliin

--- MatkahuoltoRaaputin/raaputin_0.9.py
+++ MatkahuoltoRaaputin/raaputin_0.9.py
@@ -1,87 +1,90 @@
 # -*- coding:utf-8 -*-
-# Matkahuollon skreipperi
-# Juuso Tenhunen 2.4.2013
+'''
+Luotu 2.4.2013
 
+@author: Juuso Tenhunen
+'''
 from lxml import html
-import random
 
 matkatLista = []
 
-class Matka:
-    """Yhden matka-olion luokka. Sisältää kaikki matkan tiedot, kuten myös vaihdot"""
-    depart = ""
-    arriv = ""
-    dock = ""
-    dura = ""
-    linja = ""
-    price = ""
-    vaihtolkm = 0
-    vaihdot = []
-
-    def tulosta(self):
-        print "%s | %s | %s | %s | %s | %s" % (self.depart, self.arriv, self.dock, self.dura, self.linja, self.price)
-        print "Vaihdot: %s" % (self.vaihtolkm)
-        for vaihto in self.vaihdot:
-            print "%s" % (vaihto)
-        print "\n",
-
-    def swap(self,other):
-        de = self.depart
-        ar = self.arriv
-        do = self.dock
-        du = self.dura
-        li = self.linja
-        pr = self.price
-        vlkm = self.vaihtolkm
-        v = self.vaihdot
-
-        self.depart = other.depart
-        self.arriv = other.arriv
-        self.dock = other.dock
-        self.dura = other.dura
-        self.linja = other.linja
-        self.price = other.price
-        self.vaihtolkm = other.vaihtolkm
-        self.vaihdot = other.vaihdot
-
-        other.depart = de
-        other.arriv = ar
-        other.dock = do
-        other.dura = du
-        other.linja = li
-        other.price = pr
-        other.vaihtolkm = vlkm
-        other.vaihdot = v
-
+lahto = "Ähtäri"
+#HUOM Ä=%C4, ä=%E4, Ö
+#jyväskylä = jyv%E4skyl%E4
+saapu = "Hämeenkyrö"
+paiva = "24"
+kk = "4"
+vuosi = "2013"
 
 def TeeMatka(rows):
     """luetaan skreipatut rivit, ja sijoitetaan ne 'Matka' -olioina taulukkoon attribuuttien kera"""
-    for row in rows[1:]:
-
-        matka = Matka()
-
-        matka.vaihdot = []
+    for i, row in enumerate(rows[1:]):       
 
         children = row.getchildren()
 
         if len(children) == 7:
-            vaihto = children[6].text_content()
-            matkatLista[-1].vaihdot.append("--->missä: " + " ".join(vaihto.split()))
-            matkatLista[-1].vaihtolkm = matkatLista[-1].vaihtolkm + 1
+            vaihto = " ".join(children[6].text_content().split())
+            matkatLista[-1]['vaihdot'][-1]['saapumisaika'] = vaihto.split()[-1][1:-1].split('-')[0]
+            matkatLista[-1]['vaihdot'][-1]['mihin'] = vaihto.split()[0]
+            matkatLista[-1]['vaihdot'].append({'lahtoaika': vaihto.split()[-1][1:-1].split('-')[-1],
+                                             'saapumisaika': matkatLista[-1]['saapumisaika'],
+                                             'mista': vaihto.split()[0],
+                                             'mihin': "",
+                                             'tyyppi': "",
+                                             'tunnus': "",
+                                             'vaihto_nro': matkatLista[-1]['vaihdot'][-1]['vaihto_nro'] + 1})
+            
+            matkatLista[-1]['vaihto_lkm'] = matkatLista[-1]['vaihto_lkm'] + 1
             continue
 
         if children[1].text_content().strip() ==  "":
-            matkatLista[-1].vaihdot.append("---->linja: " + children[6].text_content())
+            matkatLista[-1]['vaihdot'][-1]['mihin'] = saapu
+            matkatLista[-1]['vaihdot'][-1]['tyyppi'] = children[6].text_content().split()[0]
+            matkatLista[-1]['vaihdot'][-1]['tunnus'] = children[6].text_content()
             continue
-
-        matka.depart = children[1].text_content()
-        matka.arriv = children[3].text_content()
-        matka.dock = children[4].text_content()
-        matka.dura = children[5].text_content()
-        matka.linja = children[6].text_content()
-        matka.price = children[10].text_content()
+        
+        matka = {'indeksi': i,
+                 'lahtoaika': children[1].text_content(),
+                 'saapumisaika': children[3].text_content(),
+                 'laituri': children[4].text_content(),
+                 'kesto': children[5].text_content(),
+                 'hinta': children[10].text_content(),
+                 'vaihto_lkm': 0,
+                 'vaihdot': [
+                             # 1. vaihtoyhteys
+                             {'lahtoaika': children[1].text_content(),
+                              'saapumisaika': children[3].text_content(),
+                              'mista': lahto,
+                              'mihin': saapu,
+                              'tyyppi': children[6].text_content().split()[0],
+                              'tunnus': children[6].text_content(),
+                              'vaihto_nro': 1}
+                             ]
+                 }
 
         matkatLista.append(matka)
+        
+def Tulosta_Lista():
+    """Tulostetaan Matkalista"""
+    for matka in matkatLista:
+        print "----------------------------------------------------------------"
+        print "%s | %s | %s | %s | %s | " % (matka['lahtoaika'], matka['saapumisaika'], matka['laituri'], matka['kesto'], matka['hinta']) + "Vaihtoja: " + str(matka['vaihto_lkm'])
+        
+        for yhteys in matka['vaihdot']:
+            print str(yhteys['vaihto_nro']) + ". VaihtoYhteys"
+            print "--> %s | %s | %s | %s | %s | %s" % (yhteys['lahtoaika'], yhteys['saapumisaika'], yhteys['mista'], yhteys['mihin'], yhteys['tyyppi'], yhteys['tunnus'])
+        
+        print "----------------------------------------------------------------"
+        print "\n"
+        
+def AakkosVaihto(nimi):
+    uusi_nimi = nimi.replace('ä', "%E4")
+    uusi_nimi = uusi_nimi.replace('Ä', "%C4")
+    uusi_nimi = uusi_nimi.replace('ö', "%D6")
+    uusi_nimi = uusi_nimi.replace('Ö', "%F6")
+    uusi_nimi = uusi_nimi.replace('å', "%E5")
+    uusi_nimi = uusi_nimi.replace('Å', "%C5")
+    return uusi_nimi
 
 
 def HintaJarjestys(lista):
@@ -102,20 +105,13 @@ def ErrorMsg(errRows):
 def main():
     """skreipataan Matkahuollon sivuilta aikataulut"""
 
-    lahto = "kuopio"
-    #jyväskylä = jyv%E4skyl%E4
-    saapu = "jyv%E4skyl%E4"
-    paiva = "24"
-    kk = "4"
-    vuosi = "2013"
-
-    url = "http://www.matkahuolto.info/lippu/fi/connectionsearch?lang=fi&departureStopAreaName="+lahto+"&arrivalStopAreaName="+saapu+"&allSchedules=0&departureDay="+paiva+"&departureMonth="+kk+"&departureYear="+vuosi+"&stat=1&extl=1&search.x=-331&search.y=-383&ticketTravelType=0"
+    url = "http://www.matkahuolto.info/lippu/fi/connectionsearch?lang=fi&departureStopAreaName="+AakkosVaihto(lahto)+"&arrivalStopAreaName="+AakkosVaihto(saapu)+"&allSchedules=0&departureDay="+paiva+"&departureMonth="+kk+"&departureYear="+vuosi+"&stat=1&extl=1&search.x=-331&search.y=-383&ticketTravelType=0"
 
     # Haetaan html-tiedosto, luodaan lxml-olio:
     try:
         root = html.parse(url)
     except IOError:
-        print "Skreippaaminen ep�onnistui"
+        print "Skreippaaminen epäonnistui"
         return
 
     errRows = root.xpath("//table//tr[last()]/td[last()]//tr[1]//div[1]")
@@ -133,29 +129,7 @@ def main():
 
         TeeMatka(rows)
 
-        #Tätä voi käyttää testatakseen, että varmasti menee suuruusjärkkään
-#        for matka in matkatLista:
-#            matka.price = random.randint(1,100)
-
-        #Näillä voi testata että listassa pysyy myös samanhintaisten matkojen kohdalla
-        #lähtöaika järjestys
-#        matkatLista[0].price = 5
-#        matkatLista[3].price = 5
-#        matkatLista[5].price = 5
-#        matkatLista[6].price = 5
-
-        for matka in matkatLista:
-            matka.tulosta()
-
-        print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-        print "X NYT SITTE KAIKKI SUURUUSJÄRKÄÄN!!!! X"
-        print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX \n"
-
-        HintaJarjestys(matkatLista)
-
-
-        for matka in matkatLista:
-            matka.tulosta()
+        Tulosta_Lista()
 
 if __name__ == "__main__":
     main()
