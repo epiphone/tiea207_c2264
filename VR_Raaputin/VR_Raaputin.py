29ec23c5f39a6a2abfd2e65a7d694b3efd773b23
Lasse Wallden <Lasse.Wallden@gmail.com>
2013-04-12 12:05:02 +0300

    Nyt osataan hakea ensimmäinen hinta (Huom! Se ei ole aina halvin!)

--- VR_Raaputin/VR_Raaputin.py
+++ VR_Raaputin/VR_Raaputin.py
@@ -7,10 +7,22 @@ import urllib2
 from lxml import html
 import webbrowser
 
-# muodostetaan URL hakuehtojen perusteella
-def muodosta_url(mista, minne, lahto_tunnit, lahto_minuutit, lahto_pvm, luokka, ajan_tyyppi):
+# muodostetaan URL hakuehtojen perusteella, testaamisen vuoksi hakuehdot syötetään käsin
+#def muodosta_url(mista, minne, lahto_tunnit, lahto_minuutit, lahto_pvm, luokka, ajan_tyyppi):
+def muodosta_url():        
         
-        luokka_url = "84"
+        mista = raw_input("Mista lahdet?")
+        minne = raw_input("Minne menet?")
+        print "1=Lähtöaika, 2=Saapumisaika"
+        ajan_tyyppi = raw_input ("Tahdotko lähtö vai saapumisajan?:")
+        lahto_tunnit = raw_input("Ajan tunnit(01-23):")
+        lahto_minuutit = raw_input("Ajan minuutit:(00-59)")
+        lahto_pvm = raw_input("Lahdon paivamaara (muodossa XX.YY.ZZZZ):")
+        print "1=Aikuinen, 2=Opiskelija, 3=Eläkeläinen, 4=Juniori, 5=Varusmies, 6=Sivari"
+        luokka = raw_input("Mihin hintaluokkaan kuulut?:")
+
+        if luokka == "1":
+            luokka_url = "84"
         if luokka == "2":
             luokka_url = "85"
         if luokka == "3":
@@ -22,7 +34,8 @@ def muodosta_url(mista, minne, lahto_tunnit, lahto_minuutit, lahto_pvm, luokka, 
         if luokka == "6":
             luokka_url = "89"
         
-        ajan_tyyppi_url = "true"
+        if ajan_tyyppi == "1":
+            ajan_tyyppi_url = "true"
         if ajan_tyyppi == "2":
             ajan_tyyppi_url = "false"
         
@@ -39,19 +52,11 @@ def main():
 
     
     avaaja = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-    # testaamisen vuoksi, syötetään hakuehdot käsin
-    mista = raw_input("Mista lahdet?")
-    minne = raw_input("Minne menet?")
-    print "1=Lähtöaika, 2=Saapumisaika"
-    lahto_vai_saapuminen = raw_input ("Tahdotko lähtö vai saapumisajan?:")
-    lahto_tunnit = raw_input("Ajan tunnit(01-23):")
-    lahto_minuutit = raw_input("Ajan minuutit:(00-59)")
-    lahto_pvm = raw_input("Lahdon paivamaara (muodossa XX.YY.ZZZZ):")
-    print "1=Aikuinen, 2=Opiskelija, 3=Eläkeläinen, 4=Juniori, 5=Varusmies, 6=Sivari"
-    luokka = raw_input("Mihin hintaluokkaan kuulut?:")
 
-
-    urli = muodosta_url(mista, minne, lahto_tunnit, lahto_minuutit, lahto_pvm, luokka, lahto_vai_saapuminen)
+    #Pienten muutosten testaamisen nopeuttamiseksi, urlin muodostaminen on poissa käytöstä, ja käytössä on vakio url
+    #urli = muodosta_url(mista, minne, lahto_tunnit, lahto_minuutit, lahto_pvm, luokka, lahto_vai_saapuminen)
+    urli = muodosta_url()
+    #urli = "https://shop.vr.fi/onlineshop/JourneySearch.do?request_locale=fi&basic.fromStationVR=Kerava&basic.toStationVR=Tampere&basic.oneWay=true&basic.departureDate.hours=10&basic.departureDate.mins=10&basic.departureDate.date=31.07.2013&basic.outwardTimeSelection=false&basic.returnDate.hours=10&basic.returnDate.mins=10&basic.returnDate.date=31.07.2013&basic.returnTimeSelection=true&basic.passengerNumbers%5B0%5D.passengerType=84&basic.passengerNumbers%5B0%5D.passengerAmount=1&basic.fiRuGroup=false&basic.campaignCode="
     root = html.parse(avaaja.open(urli))
 
     rows = root.xpath("//table[@id='buyTrip_1']/tbody")
@@ -63,9 +68,12 @@ def main():
         saika = yleiset[1].text.strip()
         vaihtoja = yleiset[2].text.strip()
         kesto = yleiset[3].text.strip()
+        hinnan_paikka = yleiset[4].getchildren()
+        #print hinnan_paikka
+        hinta = hinnan_paikka[0].text.encode('utf-8')[:-5]
 
-        tuloste = "Lahtoaika: %s | Saapumisaika: %s | Vaihtoja: %s | Kesto: %s"
-        print tuloste % (laika, saika, vaihtoja, kesto)
+        tuloste = "Lahtoaika: %s | Saapumisaika: %s | Vaihtoja: %s | Kesto: %s | Hinta: %s"
+        print tuloste % (laika, saika, vaihtoja, kesto, hinta)
 
 
 if __name__ == "__main__":
