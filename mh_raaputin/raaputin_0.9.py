d1775a7a182e7567908e1578a8a1dbce2f18ae82
Juuseri <jupeliks@hotmail.com>
2013-04-22 11:04:29 +0300

    nimen vaihto

---
+++ mh_raaputin/raaputin_0.9.py
@@ -0,0 +1,191 @@
+# -*- coding:utf-8 -*-
+'''
+Luotu 2.4.2013
+
+@author: Juuso Tenhunen
+'''
+from lxml import html
+
+matkatLista = []
+
+lahto = "Kuopio"
+saapu = "Lahti"
+lahtoaika = "2013-04-24-14:59"
+# 0 = peruslippu
+# 1 = Opiskelijat, Varusmiehet, Sivarit, Lapset -50%
+# 2 = Eläkeläiset, Lehdistö, SLAHS:n jäsenet, Nuoriso -30%
+lippu_tyyppi = 0
+
+def tee_matka(rows):
+    """luetaan skreipatut rivit, ja sijoitetaan ne 'Matka' -olioina taulukkoon attribuuttien kera"""
+    for i, row in enumerate(rows[1:]):       
+
+        children = row.getchildren()
+
+        if len(children) == 7:
+            vaihto = " ".join(children[6].text_content().split())
+            matkatLista[-1]['vaihdot'][-1]['saapumisaika'] = vaihto.split()[-1][1:-1].split('-')[0]
+            matkatLista[-1]['vaihdot'][-1]['mihin'] = vaihto.split()[0]
+            matkatLista[-1]['vaihdot'].append({'lahtoaika': vaihto.split()[-1][1:-1].split('-')[-1],
+                                             'saapumisaika': matkatLista[-1]['saapumisaika'],
+                                             'mista': vaihto.split()[0],
+                                             'mihin': "",
+                                             'tyyppi': "",
+                                             'tunnus': "",
+                                             'vaihto_nro': matkatLista[-1]['vaihdot'][-1]['vaihto_nro'] + 1})
+            
+            matkatLista[-1]['vaihto_lkm'] = matkatLista[-1]['vaihto_lkm'] + 1
+            continue
+
+        if children[1].text_content().strip() ==  "":
+            matkatLista[-1]['vaihdot'][-1]['mihin'] = saapu
+            matkatLista[-1]['vaihdot'][-1]['tyyppi'] = children[6].text_content().split()[0]
+            matkatLista[-1]['vaihdot'][-1]['tunnus'] = children[6].text_content()
+            continue
+        
+        matka = {'indeksi': i,
+                 'lahtoaika': children[1].text_content(),
+                 'saapumisaika': children[3].text_content(),
+                 'laituri': children[4].text_content(),
+                 'kesto': keston_vaihto(children[5].text_content()),
+                 'hinta': laske_hinta(children[10].text_content(), children[6].text_content().split()[0]),
+                 'vaihto_lkm': 0,
+                 'vaihdot': [
+                             # 1. vaihtoyhteys
+                             {'lahtoaika': children[1].text_content(),
+                              'saapumisaika': children[3].text_content(),
+                              'mista': lahto,
+                              'mihin': saapu,
+                              'tyyppi': children[6].text_content().split()[0],
+                              'tunnus': children[6].text_content(),
+                              'vaihto_nro': 1}
+                             ]
+                 }
+
+        matkatLista.append(matka)
+        
+def tulosta_lista():
+    """Tulostetaan Matkalista"""
+    for matka in matkatLista:
+        print "----------------------------------------------------------------"
+        print "%s | %s | %s | %s | %s | " % (matka['lahtoaika'], matka['saapumisaika'], matka['laituri'], matka['kesto'], matka['hinta']) + "Vaihtoja: " + str(matka['vaihto_lkm'])
+        
+        for yhteys in matka['vaihdot']:
+            print str(yhteys['vaihto_nro']) + ". VaihtoYhteys"
+            print "--> %s | %s | %s | %s | %s | %s" % (yhteys['lahtoaika'], yhteys['saapumisaika'], yhteys['mista'], yhteys['mihin'], yhteys['tyyppi'], yhteys['tunnus'])
+        
+        print "----------------------------------------------------------------"
+        print "\n"
+        
+def aakkos_vaihto(nimi):
+    """Vaihtaa Ääkköset urlin vaatimiin muotoihin"""
+    uusi_nimi = nimi.replace('ä', "%E4")
+    uusi_nimi = uusi_nimi.replace('Ä', "%C4")
+    uusi_nimi = uusi_nimi.replace('ö', "%D6")
+    uusi_nimi = uusi_nimi.replace('Ö', "%F6")
+    uusi_nimi = uusi_nimi.replace('å', "%E5")
+    uusi_nimi = uusi_nimi.replace('Å', "%C5")
+    return uusi_nimi
+
+def keston_vaihto(aika):
+    """Vaihtaa matkan keston HHh MMmin = HH:MM (esim 5h 15min = 05:15)"""
+    kesto = aika.split()
+    tunti = kesto[0].replace("h", "")
+    
+    if len(tunti) == 1:
+        tunti = "0"+tunti
+    
+    minuutti = kesto[-1].replace("min","")
+    
+    if len(minuutti) == 1:
+        minuutti = "0" + minuutti
+        
+    return tunti + ":" + minuutti
+
+def laske_hinta(alkup_hinta, linja):
+    """Vaihtaa matkan hinnan sen mukaan, mikä hintaluokka on kyseessä (normaali, -50%, -30% etc..) VIELÄ KESKEN"""
+    try:
+        hinta = float(alkup_hinta.replace(",", "."))
+    except ValueError:
+        return 0
+    else:
+        if lippu_tyyppi == 0:
+            return hinta
+        
+        if lippu_tyyppi == 1:
+            if linja == "vakio" and hinta < 15.50:
+                return str(hinta) + " (TODO: taulukko)"
+            if linja == "pika" and hinta < 18.80:
+                return str(hinta) + " (TODO: taulukko)"
+            
+            uusihinta = hinta/2
+            
+            return round(uusihinta, 1)
+        
+        if lippu_tyyppi == 2:
+            if linja == "vakio" and hinta < 15.50:
+                return str(hinta) + " (TODO: taulukko)"
+            if linja == "pika" and hinta < 18.80:
+                return str(hinta) + " (TODO: taulukko)"
+            
+            uusihinta = hinta - (hinta*0.3)
+            
+            return round(uusihinta, 1)
+
+def error_msg(errRows):
+    """Hakee 'error_wrapper':sta errorin nimen ja tulostaa sen"""
+    print errRows[1].text_content().strip()
+    
+def hae_hinnat(url):
+    """haetaan kaikki matkan hinnat ja sijoitetaan ne dictionaryyn"""
+    rows = root.xpath("//table//tr[last()]/td[last()]//tr[1]//table[last()]//tr")
+    
+    hinta = {}
+    
+    for row in rows[2:3]: #ei huomioda kahta ekaa eikä kolmea viimeistä?
+        
+        children = row.getchildren()
+        
+        hinta[children[0].text_content()] = children[2].text_content()
+
+
+def main():
+    """skreipataan Matkahuollon sivuilta aikataulut"""
+    dd = lahtoaika.split("-")[2]
+    kk = lahtoaika.split("")[1]
+    vvvv = lahtoaika.split("-")[0]
+    
+    url = ("http://www.matkahuolto.info/lippu/fi/connectionsearch?lang=fi&departureStopAreaName=%s"
+               "&arrivalStopAreaName=%s"
+               "&allSchedules=0&departureDay=%s"
+               "&departureMonth=%s"
+               "&departureYear=%s"
+               "&stat=1&extl=1&search.x=-331&search.y=-383&ticketTravelType=0") % (lahto, saapu, dd, kk, vvvv)
+
+    # Haetaan html-tiedosto, luodaan lxml-olio:
+    try:
+        root = html.parse(url)
+    except IOError:
+        print "Skreippaaminen epäonnistui"
+        return
+
+    errRows = root.xpath("//table//tr[last()]/td[last()]//tr[1]//div[1]")
+    errRow = errRows[0]
+
+    #Jos haku tuottaa error-boxin, haetaan sen errorin nimi, eikä tehdä enää muuta
+    if errRow.attrib["class"] in ["error_wrapper"]:
+        print "Can't see shit captain!"
+        error_msg(errRows)
+
+    #jos hakuvirhettä ei tule, jatketaan normaalisti
+    else:
+
+        rows = root.xpath("//table//tr[last()]/td[last()]//tr[1]//table[2]//tr")
+
+        tee_matka(rows)
+
+        tulosta_lista()
+
+if __name__ == "__main__":
+    main()
+
---
+++ mh_raaputin/raaputin_alpha.py
@@ -0,0 +1,146 @@
+# -*- coding:utf-8 -*-
+'''
+Luotu 16.4.2013
+
+@author: Juuso Tenhunen
+'''
+from lxml import html
+
+matkat_lista = []
+
+class MHScraper:
+    """luokka raaputtimelle"""
+    def __init__(self):
+        """konstruktori"""
+        pass
+    
+    def hae_matka(self, mista, mihin, lahtoaika=None, saapumusaika=None):
+        """luetaan skreipatut rivit, ja sijoitetaan ne 'Matka' -olioina taulukkoon attribuuttien kera"""
+        
+        dd = lahtoaika.split("-")[2]
+        kk = lahtoaika.split("-")[1]
+        vvvv = lahtoaika.split("-")[0]
+    
+        url = ("http://www.matkahuolto.info/lippu/fi/"
+               "connectionsearch?lang=fi&departureStopAreaName=%s"
+               "&arrivalStopAreaName=%s"
+               "&allSchedules=0&departureDay=%s"
+               "&departureMonth=%s"
+               "&departureYear=%s"
+               "&stat=1&extl=1&search.x=-331&search.y=-383&ticketTravelType=0") % (mista, mihin, dd, kk, vvvv)
+        
+        # Haetaan html-tiedosto, luodaan lxml-olio:
+        try:
+            root = html.parse(url)
+        except IOError:
+            print "Skreippaaminen epäonnistui"
+            return
+
+        err_rows = root.xpath("//table//tr[last()]/td[last()]//tr[1]//div[1]")
+        err_row = err_rows[0]
+
+        #Jos haku tuottaa error-boxin, haetaan sen errorin nimi, eikä tehdä enää muuta
+        if err_row.attrib["class"] in ["error_wrapper"]:
+            return error_msg(err_rows)
+        
+
+        #jos hakuvirhettä ei tule, jatketaan normaalisti
+        else:
+
+            rows = root.xpath("//table//tr[last()]/td[last()]//tr[1]//table[2]//tr")
+    
+            for i, row in enumerate(rows[1:]):       
+
+                children = row.getchildren()
+
+                if len(children) == 7:
+                    vaihto = " ".join(children[6].text_content().split())
+                    matkat_lista[-1]['vaihdot'][-1]['saapumisaika'] = vaihto.split()[-1][1:-1].split('-')[0]
+                    matkat_lista[-1]['vaihdot'][-1]['mihin'] = vaihto.split()[0]
+                    matkat_lista[-1]['vaihdot'].append({'lahtoaika': vaihto.split()[-1][1:-1].split('-')[-1],
+                                                       'saapumisaika': matkat_lista[-1]['saapumisaika'],
+                                                       'mista': vaihto.split()[0],
+                                                       'mihin': "",
+                                                       'tyyppi': "",
+                                                       'tunnus': "",
+                                                       'vaihto_nro': matkat_lista[-1]['vaihdot'][-1]['vaihto_nro'] + 1})
+            
+                    matkat_lista[-1]['vaihto_lkm'] = matkat_lista[-1]['vaihto_lkm'] + 1
+                    continue
+
+                if children[1].text_content().strip() ==  "":
+                    matkat_lista[-1]['vaihdot'][-1]['mihin'] = mihin
+                    matkat_lista[-1]['vaihdot'][-1]['tyyppi'] = children[6].text_content().split()[0]
+                    matkat_lista[-1]['vaihdot'][-1]['tunnus'] = children[6].text_content()
+                    continue
+        
+                matka = {'indeksi': i, 
+                         'lahtoaika': children[1].text_content(),
+                         'saapumisaika': children[3].text_content(),
+                         'laituri': children[4].text_content(),
+                         'kesto': keston_vaihto(children[5].text_content()),
+                         'hinta': children[10].text_content(),
+                         'vaihto_lkm': 0,
+                         'vaihdot': [
+                                # 1. vaihtoyhteys
+                                {'lahtoaika': children[1].text_content(),
+                                 'saapumisaika': children[3].text_content(),
+                                 'mista': mista,
+                                 'mihin': mihin,
+                                 'tyyppi': children[6].text_content().split()[0],
+                                 'tunnus': children[6].text_content(),
+                                 'vaihto_nro': 1}
+                                 ]
+                                }
+
+                matkat_lista.append(matka)
+            
+            return matkat_lista
+
+def aakkos_vaihto(nimi):
+    """Vaihtaa Ääkköset urlin vaatimiin muotoihin"""
+    uusi_nimi = nimi.replace('ä', "%E4")
+    uusi_nimi = uusi_nimi.replace('Ä', "%C4")
+    uusi_nimi = uusi_nimi.replace('ö', "%D6")
+    uusi_nimi = uusi_nimi.replace('Ö', "%F6")
+    uusi_nimi = uusi_nimi.replace('å', "%E5")
+    uusi_nimi = uusi_nimi.replace('Å', "%C5")
+    return uusi_nimi
+
+def keston_vaihto(aika):
+    """Vaihtaa matkan keston HHh MMmin = HH:MM (esim 5h 15min = 05:15)"""
+    kesto = aika.split()
+    tunti = kesto[0].replace("h", "")
+    
+    if len(tunti) == 1:
+        tunti = "0"+tunti
+    
+    minuutti = kesto[-1].replace("min","")
+    
+    if len(minuutti) == 1:
+        minuutti = "0" + minuutti
+        
+    return tunti + ":" + minuutti
+
+def error_msg(err_rows):
+    """Hakee 'error_wrapper':sta errorin nimen ja tulostaa sen"""
+    print err_rows[1].text_content().strip()
+    
+def hae_hinnat(url):
+    """haetaan kaikki matkan hinnat ja sijoitetaan ne dictionaryyn"""
+    
+    try:
+        root = html.parse(url)
+    except IOError:
+        print "Skreippaaminen epäonnistui"
+        return
+    
+    rows = root.xpath("//table//tr[last()]/td[last()]//tr[1]//table[last()]//tr")
+    
+    hinta = {}
+    
+    for row in rows[2:3]: #ei huomioda kahta ekaa eikä kolmea viimeistä?
+        
+        children = row.getchildren()
+        
+        hinta[children[0].text_content()] = children[2].text_content()
