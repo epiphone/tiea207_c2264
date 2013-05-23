57d97649afae702e4ce42a064d7658c9c9be03b4
Lasse Wallden <Lasse.Wallden@gmail.com>
2013-05-23 11:55:41 +0300

    lisenssi lisätty myös mainiin

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ Reititykset, templatet yms.
 Aleksi Pekkala
 """
 
+"""
+ This file is part of Millä Matkaan.
+
+    Millä Matkaan is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Millä Matkaan is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Millä matkaan.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 import web
 from scraper_wrapper import ScraperWrapper
 from datetime import datetime, timedelta
